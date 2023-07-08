f66c40faf85878046622db5827650998bc9c8e01
Sushmita <sushmitakumari363@gmail.com>
2023-07-08 21:37:22 +0530

    Stylling Updated

--- src/Component/ResturantDetail.jsx
+++ src/Component/ResturantDetail.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useResturant } from "../Context/ResturantContext";
 import { useEffect, useState } from "react";
 import { ReviewModal } from "./ReviewModal";
+import StarRateIcon from "@mui/icons-material/StarRate";
 
 export const ResturantDetail = () => {
   const { resturantStates, resturantDispatch } = useResturant();
@@ -65,11 +66,26 @@ export const ResturantDetail = () => {
       </Box>
       <Box>
         <Typography variant="h5">Reviews</Typography>
-        {resturantStates?.resturantDetail?.ratings?.map((rating) => (
-          <Box>
-            <img src={rating.pp} alt="profile" />
-            <Typography>{rating.revName}</Typography>
-          </Box>
+        {resturantStates?.resturantDetail?.map((resturant) => (
+          <div key={resturant.id}>
+            {resturant?.ratings?.map((rating, idx) => (
+              <Box key={idx} sx={{ display: "flex", flexDirection: "row" }}>
+                <Box sx={{ display: "flex", flexDirection: "column" }}>
+                  <img src={rating.pp} alt="profile" />
+                  <Typography>{rating.revName}</Typography>
+                </Box>
+
+                <Typography>{rating.comment}</Typography>
+                <Button
+                  variant="contained"
+                  startIcon={<StarRateIcon />}
+                  sx={{ height: "20px", width: "5px" }}
+                >
+                  {rating.rating}
+                </Button>
+              </Box>
+            ))}
+          </div>
         ))}
       </Box>
     </Box>
